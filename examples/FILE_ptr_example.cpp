8480b8ad82cfb10bdf150c60edc2d1e35c62f16d
2s Creative <2screative@2sCreative1s-MacBook-Pro.local>
2017-06-07 18:31:04 -0700

    adoc

--- examples/FILE_ptr_example.cpp
+++ examples/FILE_ptr_example.cpp
@@ -1,11 +1,3 @@
-//Copyright (c) 2017 Emil Dotchevski and Reverge Studios, Inc.
-
-//Distributed under the Boost Software License, Version 1.0. (See accompanying
-//file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-//This program demonstrates the use of result_traits_ptr, which is based on the
-//observation that functions returning pointers (always?) return 0 on error.
-
 #include <boost/noexcept/result_traits_ptr.hpp>
 #include <boost/noexcept/try.hpp>
 #include <boost/noexcept/throw.hpp>
@@ -15,28 +7,22 @@ using namespace boost::noexcept_;
 
 struct file_open_error: std::exception { };
 
-//Return the FILE pointer on success or throw_s file_open_error.
-FILE *
-open_file( char const * name )
-	{
+//Returns the FILE pointer on success or throw_s file_open_error.
+FILE * open_file( char const * name ) {
 	if( FILE * f=fopen(name,"rb") )
 		return f;
 	else
 		return throw_(file_open_error());
-	}
-int
-main()
-	{
-	if( auto tr=try_(open_file("file_name")) )
-		{
+}
+
+int main() {
+	if( auto tr=try_(open_file("file_name")) ) {
 		//Success! Get the FILE pointer:
 		FILE * f=tr.value();
 		//Use the file, then close it
 		fclose(f);
-		}
-	else if( file_open_error * err=tr.catch_<file_open_error>() )
-		{
+	} else if( file_open_error * err=tr.catch_<file_open_error>() ) {
 		//The file failed to open! Handle error.
-		}
-	return 0;
 	}
+	return 0;
+}
