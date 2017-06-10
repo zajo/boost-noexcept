a6dea2efd194e8f0cde2311a631684e5a7f9e4c2
Emil Dotchevski <emil@revergestudios.com>
2017-06-10 00:30:31 -0700

    documentation

--- include/boost/noexcept.hpp
+++
@@ -1,7 +0,0 @@
-//Copyright (c) 2017 Emil Dotchevski and Reverge Studios, Inc.
-
-//Distributed under the Boost Software License, Version 1.0. (See accompanying
-//file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#include <boost/noexcept/throw.hpp>
-#include <boost/noexcept/try.hpp>
