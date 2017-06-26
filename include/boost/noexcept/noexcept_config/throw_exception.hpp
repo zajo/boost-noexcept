c1050073e38ffb11b49720f0d74027d3da6e2d75
Emil Dotchevski <emildotchevski@gmail.com>
2017-06-25 19:21:37 -0700

    BOOST_NOEXCEPT_NO_EXCEPTIONS

--- include/boost/noexcept/noexcept_config/throw_exception.hpp
+++ include/boost/noexcept/noexcept_config/throw_exception.hpp
@@ -6,29 +6,33 @@
 #ifndef UUID_FB56599248D611E78192CFCF7E4E887A
 #define UUID_FB56599248D611E78192CFCF7E4E887A
 
-#ifndef BOOST_NOEXCEPT_THROW_EXCEPTION
-#include <boost/throw_exception.hpp>
-#define BOOST_NOEXCEPT_THROW_EXCEPTION ::boost::throw_exception
+#ifdef BOOST_NOEXCEPT_NO_EXCEPTIONS
+#   define BOOST_NOEXCEPT_THROW_EXCEPTION(x) std::terminate()
+#else
+#   ifndef BOOST_NOEXCEPT_THROW_EXCEPTION
+#       include <boost/throw_exception.hpp>
+#       define BOOST_NOEXCEPT_THROW_EXCEPTION ::boost::throw_exception
+#   endif
 #endif
 
 #if !defined(BOOST_NOEXCEPT_NORETURN)
-#  if defined(_MSC_VER)
-#    define BOOST_NOEXCEPT_NORETURN __declspec(noreturn)
-#  elif defined(__GNUC__)
-#    define BOOST_NOEXCEPT_NORETURN __attribute__ ((__noreturn__))
-#  elif defined(__has_attribute) && defined(__SUNPRO_CC)
-#    if __has_attribute(noreturn)
-#      define BOOST_NOEXCEPT_NORETURN [[noreturn]]
-#    endif
-#  elif defined(__has_cpp_attribute) 
-#    if __has_cpp_attribute(noreturn)
-#      define BOOST_NOEXCEPT_NORETURN [[noreturn]]
-#    endif
-#  endif
+#   if defined(_MSC_VER)
+#       define BOOST_NOEXCEPT_NORETURN __declspec(noreturn)
+#   elif defined(__GNUC__)
+#       define BOOST_NOEXCEPT_NORETURN __attribute__ ((__noreturn__))
+#   elif defined(__has_attribute) && defined(__SUNPRO_CC)
+#       if __has_attribute(noreturn)
+#           define BOOST_NOEXCEPT_NORETURN [[noreturn]]
+#       endif
+#   elif defined(__has_cpp_attribute) 
+#       if __has_cpp_attribute(noreturn)
+#           define BOOST_NOEXCEPT_NORETURN [[noreturn]]
+#       endif
+#   endif
 #endif
 
 #if !defined(BOOST_NOEXCEPT_NORETURN)
-#  define BOOST_NOEXCEPT_NORETURN
+#   define BOOST_NOEXCEPT_NORETURN
 #endif
 
 #endif
